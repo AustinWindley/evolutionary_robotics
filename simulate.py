b86573836f9122f0d3c86d5dfd22c13f3fca5f95
Austin <Austin@LAPTOP-4VS2LIO5>
2026-01-14 13:30:05 -0500

    For loop stepSimulation

--- simulate.py
+++ simulate.py
@@ -1,5 +1,11 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+for x in (range(1000)):
+    p.stepSimulation()
+    print(x)
+    time.sleep(1/600)
 p.disconnect()
 
