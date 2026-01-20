9ef57cd8876ba7d1991bac1b715b3eb32385e61f
Austin <Austin@LAPTOP-4VS2LIO5>
2026-01-20 11:25:23 -0500

    Finished base code

--- simulate.py
+++ simulate.py
@@ -2,10 +2,10 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 for x in (range(1000)):
     p.stepSimulation()
-    print(x)
-    time.sleep(1/600)
+    # print(x)
+    time.sleep(1/60)
 p.disconnect()
 
