5c45f2abbe6b851ffd7693dcf851361232513344
Austin <Austin@LAPTOP-4VS2LIO5>
2026-02-19 17:15:54 -0500

    finished motors

--- analyze.py
+++ analyze.py
@@ -1,8 +1,15 @@
-import numpy
-import matplotlib.pyplot
-backLegSensorValues = numpy.load("data/backLegSensorData.npy")
-frontLegSensorValues = numpy.load("data/frontLegSensorData.npy")
-matplotlib.pyplot.plot(backLegSensorValues, linewidth = 4,label = "Back Leg")
-matplotlib.pyplot.plot(frontLegSensorValues, label = "Front Leg")
-matplotlib.pyplot.legend()
-matplotlib.pyplot.show()
+import numpy as np
+import matplotlib.pyplot as plt
+# backLegSensorValues = np.load("data/backLegSensorData.npy")
+# frontLegSensorValues = np.load("data/frontLegSensorData.npy")
+BackLeg_targetAngles = np.load("data/BackLeg_targetAngles.npy")
+FrontLeg_targetAngles = np.load("data/FrontLeg_targetAngles.npy")
+
+# plt.plot(backLegSensorValues, linewidth = 4,label = "Back Leg")
+# plt.plot(frontLegSensorValues, label = "Front Leg")
+plt.plot(BackLeg_targetAngles, label = "BackLeg_targetAngles")
+plt.plot(FrontLeg_targetAngles, label = "FrontLeg_targetAngles")
+plt.xlabel("Steps")
+plt.ylabel("Value in Radians")
+plt.legend()
+plt.show()
