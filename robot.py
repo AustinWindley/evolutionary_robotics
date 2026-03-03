43a9e2f28436da2f6550fa03bb1fdffc0e8711af
Austin <Austin@LAPTOP-4VS2LIO5>
2026-03-03 15:03:39 -0500

    Step 59

--- robot.py
+++ robot.py
@@ -28,6 +28,8 @@ class ROBOT:
             self.motors[jointName] = MOTOR(jointName)
     
     def Act(self, t):
+        for neuronName in self.nn.Get_Neuron_Names():
+            print(neuronName)
         for motor in self.motors.values():
             motor.Set_Value(self.robotId, t)
     
