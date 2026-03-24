f04c3a7a8571195c6c1d37c78fdeeac162561c51
Austin <Austin@LAPTOP-4VS2LIO5>
2026-03-24 18:28:46 -0400

    Step 105

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -17,17 +17,14 @@ class PARALLEL_HILL_CLIMBER:
 
     def Evolve(self):
         # self.parent.Evaluate("GUI")
-        for i in self.parents.keys():
-            self.parents[i].Start_Simulation("DIRECT")
-        for i in self.parents.keys():
-            self.parents[i].Wait_For_Simulation_To_End()
+        self.Evaluate(self.parents)
         for currentGeneration in range(c.numberOfGenerations):
             self.Evolve_For_One_Generation()
 
     def Evolve_For_One_Generation(self):
         self.Spawn()
-        # self.Mutate()
-        # self.child.Evaluate("DIRECT")
+        self.Mutate()
+        self.Evaluate(self.children)
         # print(f"\nparent: {self.parent.fitness} child: {self.child.fitness}")
         # self.Select()
 
@@ -39,7 +36,8 @@ class PARALLEL_HILL_CLIMBER:
             self.nextAvailableID += 1        
     
     def Mutate(self):
-        self.child.Mutate()
+        for i in self.children.keys():
+            self.children[i].Mutate()
     
     def Select(self):
         if (self.parent.fitness > self.child.fitness):
@@ -48,3 +46,9 @@ class PARALLEL_HILL_CLIMBER:
     def Show_Best(self):
         # self.parent.Evaluate("GUI")
         pass
+
+    def Evaluate(self, solutions):
+        for i in solutions.keys():
+            solutions[i].Start_Simulation("DIRECT")
+        for i in solutions.keys():
+            solutions[i].Wait_For_Simulation_To_End()
