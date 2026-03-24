57c91131f56e0df3af38705a9852cc16db284f48
Austin <Austin@LAPTOP-4VS2LIO5>
2026-03-24 13:29:13 -0400

    step 14

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -49,14 +50,17 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name = 2, linkName="FrontLeg")
     pyrosim.Send_Motor_Neuron(name = 3, jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name = 4, jointName = "Torso_FrontLeg")
-    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 3, weight = 1.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 3, weight = 4.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 4, weight = 1.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 4, weight = 12.0)
-
+    # pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 3, weight = 1.0)
+    # pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 3, weight = 4.0)
+    # pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 4, weight = 1.0)
+    # pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 4, weight = 12.0)
+    for i in [0,1,2]:
+        for j in [3,4]:
+            pyrosim.Send_Synapse(sourceNeuronName=i, targetNeuronName=j, weight=random.uniform(-1.0,1.0))
 
     pyrosim.End()
 
 Create_World()
 Generate_Body()
 Generate_Brain()
+
