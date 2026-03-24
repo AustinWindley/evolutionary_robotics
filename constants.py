e21ef27eabc4624f4c3bd89b2360aef0e3cae853
Austin <Austin@LAPTOP-4VS2LIO5>
2026-03-24 13:34:34 -0400

    finished randomsearch

--- constants.py
+++ constants.py
@@ -1,14 +1,10 @@
 import numpy as np
 
-run_length = 1000
+run_length = 200
 
 amplitude = np.pi/4
 frequency = 63
 phaseOffset = np.pi
-
-# FrontLeg_amplitude = np.pi/4
-# FrontLeg_frequency = 63
-# FrontLeg_phaseOffset = 0
 
 backLegSensorValues = np.zeros(run_length)
 frontLegSensorValues = np.zeros(run_length)
