eb155a0fe27be822e587f7a9aec4512c2ae51134
Austin <Austin@LAPTOP-4VS2LIO5>
2026-03-24 18:19:28 -0400

    Step 89

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -25,17 +25,18 @@ class PARALLEL_HILL_CLIMBER:
             self.Evolve_For_One_Generation()
 
     def Evolve_For_One_Generation(self):
-        # self.Spawn()
+        self.Spawn()
         # self.Mutate()
         # self.child.Evaluate("DIRECT")
         # print(f"\nparent: {self.parent.fitness} child: {self.child.fitness}")
         # self.Select()
-        pass
 
     def Spawn(self):
-        self.child = copy.deepcopy(self.parent)
-        self.child.Set_ID(self.nextAvailableID)
-        self.nextAvailableID += 1
+        self.children = {}
+        for i in self.parents.keys():
+            self.children[i] = copy.deepcopy(self.parents[i])
+            self.children[i].Set_ID(self.nextAvailableID)
+            self.nextAvailableID += 1        
     
     def Mutate(self):
         self.child.Mutate()
